6bc9312aeef6dab7ee360ad27cdcb567ec301089
Jinhyoung Kim <jinhyoung0213@gmail.com>
2022-06-07 17:35:10 +0900

    second

--- src/main/java/co/jin/prj/DataSource.java
+++ src/main/java/co/jin/prj/DataSource.java
@@ -7,17 +7,37 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class DataSource {
 
-	public DataSource() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+public class DataSource { // 싱글톤 Class로 만듬
+	   private static SqlSessionFactory sqlSessionFactory;
+	   private DataSource() {}
+	   
+	   public static SqlSessionFactory getInstance() {
+	      try {
+	         String resource = "mybatis-config.xml";
+	         InputStream inputStream = Resources.getResourceAsStream(resource);
+	         sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      }
+	      return sqlSessionFactory;
+	   }
 	}
-
-}
+//public class DataSource {
+//	
+//	private static SqlSessionFactory sqlSessionFactory;
+//	private DataSource() {}
+//
+//	public static SqlSessionFactory getInstance() {
+//		try {
+//			String resource = "mybatis-config.xml";
+//			InputStream inputStream;
+//			inputStream = Resources.getResourceAsStream(resource);
+//			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return sqlSessionFactory;
+//	}
+//
+//}
